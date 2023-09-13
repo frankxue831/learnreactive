a2427a249b62b60c73641e141b3bcffc65f5de47
Fengxiang Xue <47923440+frankxue831@users.noreply.github.com>
2023-09-13 22:48:35 +0800

    Initial commit
    This commit initializes the project by adding:
    
    - `.gitignore` - Specifies intentionally untracked files
    - `build.gradle` - Gradle build configuration file
    - `gradle/wrapper` - Wrapper files for Gradle
    - `gradlew` - Gradle wrapper script for Unix
    - `gradlew.bat` - Gradle wrapper script for Windows
    - `settings.gradle` - Gradle settings file
    - `src/main/java/com/frank/learnreactive/LearnreactiveApplication.java` - Main application class
    - `src/main/resources/application.properties` - Application configuration
    - `src/test/java/com/frank/learnreactive/LearnreactiveApplicationTests.java` - Example test case

---
+++ src/test/java/com/frank/learnreactive/LearnreactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frank.learnreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnreactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
