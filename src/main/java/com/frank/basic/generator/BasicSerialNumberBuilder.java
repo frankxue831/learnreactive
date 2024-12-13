c389c0982fdbc76518383d8bd1d888d911019780
Fengxiang Xue <47923440+frankxue831@users.noreply.github.com>
2024-12-13 22:31:06 +0800

    Update Gradle and Java configurations; enhance BasicSerialNumberBuilder implementation

--- src/main/java/com/frank/basic/generator/BasicSerialNumberBuilder.java
+++ src/main/java/com/frank/basic/generator/BasicSerialNumberBuilder.java
@@ -1,2 +1,46 @@
-package com.frank.basic.generator;public class BasicSerialNumberBuilder {
+package com.frank.basic.generator;
+
+public class BasicSerialNumberBuilder implements SerialNumberBuilder {
+
+  private String constantString;
+  private int creditNum;
+  private String appCode;
+
+  public BasicSerialNumberBuilder() {
+  }
+
+  public SerialNumberBuilder withConstantString(String constant) {
+    constantString = constant;
+    return this;
+  }
+
+  public SerialNumberBuilder withCreditNum(int creditnum) {
+    creditNum = creditnum;
+    return this;
+  }
+
+  public SerialNumberBuilder withAppCode(String appcode) {
+    appCode = appcode;
+    return this;
+  }
+
+  @Override
+  public BasicSerialNumber build() {
+    BasicSerialNumber serialNumber = new BasicSerialNumber();
+    serialNumber.setConstantString(constantString);
+    serialNumber.setCreditNum(creditNum);
+    serialNumber.setAppCode(appCode);
+    return serialNumber;
+  }
+
+  public static void main(String[] args) {
+    BasicSerialNumberBuilder builder = new BasicSerialNumberBuilder();
+    builder.withConstantString("ABC");
+    builder.withCreditNum(123);
+    builder.withAppCode("XYZ");
+    BasicSerialNumber serialNumber = builder.build();
+    System.out.println(serialNumber);
+  }
+
 }
+
